1396f63678ef58001bf26d844f53e5d8cc192fc4
Billadom0123 <3233405848@qq.com>
2025-09-05 21:34:05 +0800

    Initial Commit

---
+++ src/main/java/com/example/web/filmforum/FilmForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.web.filmforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmForumApplication.class, args);
+    }
+
+}
